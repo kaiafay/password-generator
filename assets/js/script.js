f7cd85b9119c6d6464dd6c06d3f4cac3e23614ba
Kaia Scheirman <kfscheirman18@gmail.com>
2022-02-15 20:41:34 -0800

    separate criteria into individual functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,13 +6,24 @@ var writePassword = function () {
 
 // function that generates password
 var generatePassword = function () {
-  // length of password criteria
-  var lengthPrompt = window.prompt("What length would you like your password to be? (8-128 characters)");
-  if (lengthPrompt < 8 || lengthPrompt > 128) {
-    window.alert("You must pick a number between 8 and 128.");
-    return generatePassword();
-  };
+  passwordLength();
+  passwordLowercase();
+  passwordUppercase();
+  passwordNumeric();
+  passwordSpecial();
+};
 
+var passwordLength = function() {
+  // prompt for password length
+  var lengthPrompt = window.prompt("What length would you like your password to be? (8-128 characters)");
+  // validate the number input
+    if (lengthPrompt < 8 || lengthPrompt > 128) {
+      window.alert("You must pick a number between 8 and 128.");
+      return passwordLength();
+  };
+};
+
+var passwordLowercase = function() {
   // lowercase character criteria
   var lowercasePrompt = window.prompt("Would you like to include lowercase characters? Please type 'yes' or 'no'.");
   // make input lowercase to validate 
@@ -20,9 +31,11 @@ var generatePassword = function () {
 
   if (lowercasePrompt !== 'yes' && lowercasePrompt !== 'no') {
     window.alert("You must choose 'yes' or 'no'.");
-    return generatePassword();
+    return passwordLowercase();
   };
+};
 
+var passwordUppercase = function() {
   // uppercase character criteria
   var uppercasePrompt = window.prompt("Would you like to include uppercase characters? Please type 'yes or 'no'.");
   // make input lowercase to validate
@@ -30,18 +43,23 @@ var generatePassword = function () {
 
   if (uppercasePrompt !== 'yes' && uppercasePrompt !== 'no') {
     window.alert("You must choose 'yes' or 'no'.");
-    return generatePassword();
+    return passwordUppercase();
   };
+};
 
+var passwordNumeric = function() {
+  // numeric character criteria
   var numericPrompt = window.prompt("Would you like to include numeric characters? Please type 'yes' or 'no'.");
   // make input lowercase to validate
   numericPrompt = numericPrompt.toLowerCase();
 
   if (numericPrompt !== 'yes' && numericPrompt !== 'no') {
     window.alert("You must choose 'yes' or 'no'.");
-    return generatePassword();
+    return passwordNumeric();
   };
+};
 
+var passwordSpecial = function() {
   // special character criteria
   var specialPrompt = window.prompt("Would you like to include special characters? Please type 'yes' or 'no'.");
   // make input lowercase to validate
@@ -49,10 +67,11 @@ var generatePassword = function () {
 
   if (specialPrompt !== 'yes' && specialPrompt !== 'no') {
     window.alert("You must choose 'yes' or 'no'.");
-    return generatePassword();
+    return passwordSpecial();
   };
-
 };
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
