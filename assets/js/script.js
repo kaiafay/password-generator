e015769f6c3986c203df7debb597185d71bd4622
Kaia Scheirman <kfscheirman18@gmail.com>
2022-02-16 17:35:45 -0800

    add character arrays and return statements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
+// array of characters
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+var UpperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialChar = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
+
 // function that writes the password
 var writePassword = function () {
   generatePassword();
@@ -22,26 +28,41 @@ var passwordLength = function() {
       window.alert("You must pick a number between 8 and 128.");
       return passwordLength();
   };
+
+  // return password length
+  return lengthPrompt;
 };
 
 var passwordLowercase = function() {
   // lowercase character criteria
   var lowercasePrompt = window.confirm("Would you like to include lowercase characters? Please press 'OK' for yes or 'Cancel' for no.");
+  
+  // return boolean value
+  return lowercasePrompt;
 };
 
 var passwordUppercase = function() {
   // uppercase character criteria
   var uppercasePrompt = window.confirm("Would you like to include uppercase characters? Please press 'OK' for yes or 'Cancel' for no.");
+
+  // return boolean value
+  return uppercasePrompt;
 };
 
 var passwordNumeric = function() {
   // numeric character criteria
   var numericPrompt = window.confirm("Would you like to include numeric characters? Please press 'OK' for yes or 'Cancel' for no.");
+
+  // return boolean value
+  return numericPrompt;
 };
 
 var passwordSpecial = function() {
   // special character criteria
   var specialPrompt = window.confirm("Would you like to include special characters? Please press 'OK' for yes or 'Cancel' for no.");
+  
+  // return boolean value
+  return specialPrompt;
 };
 
 
