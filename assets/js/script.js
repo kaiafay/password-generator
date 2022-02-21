88d13a4eec45a90dc2ec407c14f096606723084f
Kaia Scheirman <kfscheirman18@gmail.com>
2022-02-20 18:43:18 -0800

    refactor/reorganize code to fix bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,87 +4,117 @@ var UpperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var specialChar = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
 
-// array for possible characters
-var possibleChar = [];
+// function that holds all the prompts
+var prompts = function() {
 
-// array for resulting characters
-var result = [];
-
-var passwordLength = function() {
-  // prompt for password length
-  var lengthPrompt = window.prompt("What length would you like your password to be? (8-128 characters)");
-  // validate the number input
-    if (lengthPrompt < 8 || lengthPrompt > 128) {
+  // password length criteria
+  var passwordLength =  window.prompt("What length would you like your password to be? (8-128 characters)");
+    // validate the number input
+    if (passwordLength < 8 || passwordLength > 128) {
       window.alert("You must pick a number between 8 and 128.");
-      return passwordLength();
+      return prompts();
       // checks to see if the input is not a number 
-    } else if (isNaN(lengthPrompt)) {
+    } else if (isNaN(passwordLength)) {
       window.alert("Invalid input! Please pick a number.");
-      return passwordLength();
+      return prompts();
     };
 
-  // return user's value
-  return lengthPrompt;
-};
-
-var passwordLowercase = function() {
   // lowercase character criteria
-  var lowercasePrompt = window.confirm("Would you like to include lowercase characters? Please press 'OK' for yes or 'Cancel' for no.");
-  
-  // returns user's value
-  return lowercasePrompt;
-};
+  var passwordLowercase = window.confirm("Would you like to include lowercase characters? Please press 'OK' for yes or 'Cancel' for no.");
 
-var passwordUppercase = function() {
   // uppercase character criteria
-  var uppercasePrompt = window.confirm("Would you like to include uppercase characters? Please press 'OK' for yes or 'Cancel' for no.");
+  var passwordUppercase = window.confirm("Would you like to include uppercase characters? Please press 'OK' for yes or 'Cancel' for no.");
 
-  // returns user's value
-  return uppercasePrompt;
-};
-
-var passwordNumeric = function() {
   // numeric character criteria
-  var numericPrompt = window.confirm("Would you like to include numeric characters? Please press 'OK' for yes or 'Cancel' for no.");
+  var passwordNumeric = window.confirm("Would you like to include numeric characters? Please press 'OK' for yes or 'Cancel' for no.");
 
-  // returns user's value
-  return numericPrompt;
+  // special character criteria 
+  var passwordSpecial = window.confirm("Would you like to include special characters? Please press 'OK' for yes or 'Cancel' for no.");
+
+  // checks if user doesn't include any character types 
+  if (passwordLowercase === false && passwordUppercase === false && passwordNumeric === false && passwordSpecial === false) {
+    alert("You must select at least one character type!");
+    return generatePassword();
+  };
+
+  // object to store user's answers
+  var userInput = {
+    length: passwordLength,
+    includeLower: passwordLowercase,
+    includeUpper: passwordUppercase,
+    includeNumeric: passwordNumeric,
+    includeSpecial: passwordSpecial,
+  };
+
+  // returns user's answers
+  return userInput;
 };
 
-var passwordSpecial = function() {
-  // special character criteria
-  var specialPrompt = window.confirm("Would you like to include special characters? Please press 'OK' for yes or 'Cancel' for no.");
-  
-  // returns user's value
-  return specialPrompt;
+// function that gets a random element from an array
+function randomize(arr) {
+  // gets a random number based off the array length
+  var randomIndex = Math.floor(Math.random() * arr.length);
+  // picks the element in the array that corresponds to the random number
+  var randomEl = arr[randomIndex];
+  // returns the randomized element
+  return randomEl;
 };
-
-// checks if user doesn't include any character types 
-// if (passwordLowercase === false && passwordUppercase === false && passwordNumeric === false && passwordSpecial === false) {
-//   alert("You must select at least one character type!");
-//   return generatePassword();
-// }
 
 // function that generates password
 var generatePassword = function () {
+  var options = prompts();
 
-  // runs prompts
-  var passLength = passwordLength();
-  var includeLower = passwordLowercase();
-  var includeUpper = passwordUppercase();
-  var includeNumeric = passwordNumeric();
-  var includeSpecial = passwordSpecial();
+  // array for possible characters
+  var possibleChar = [];
 
-  // checks if user doesn't include any character types 
-if (includeLower === false && includeUpper === false && includeNumeric === false && includeSpecial === false) {
-  alert("You must select at least one character type!");
-  return generatePassword();
-}
- 
-  // for loop for password length of possible characters
-  for (let i=0; i < length -1; i++) {
-    var randomSpot = possibleChar[i].length
-    result.push(possibleChar[i][Math.floor(Math.random() * randomSpot)]);
+  // array for resulting characters
+  var result = [];
+
+  // array to ensure one of each selected character is included in the password
+  var requiredChar = [];
+
+  // adds lowercase characters array to possible characters array if selected by user
+  if (options.includeLower === true) {
+    possibleChar = possibleChar.concat(lowerCase);
+    // pushes random lowercase character to required characters array
+    requiredChar.push(randomize(lowerCase));
+  }
+
+  // adds uppercase characters array to possible characters array if selected by user
+  if (options.includeUpper === true) {
+    possibleChar = possibleChar.concat(UpperCase);
+    // pushes random uppercase character to required characters array
+    requiredChar.push(randomize(UpperCase));
+  }
+
+  // adds numeric characters array to possible characters array if selected by user
+  if (options.includeNumeric === true) {
+    possibleChar = possibleChar.concat(numbers);
+    // pushes random numeric character to rquired characters array
+    requiredChar.push(randomize(numbers));
+  }
+
+  // adds special characters array to possible characters array if selected by user
+  if (options.includeSpecial === true) {
+    possibleChar = possibleChar.concat(specialChar);
+    // pushes random special character to required characters array
+    requiredChar.push(randomize(specialChar));
+  }
+
+  console.log(requiredChar);
+
+  // for loop that picks characters from possible characters array
+  for (let i=0; i < options.length; i++) {
+    // randomly picks a character from the possible characters array
+    var character = randomize(possibleChar)
+
+    // pushes the random character to the result array for the password
+    result.push(character)
+  };
+
+  // for loop that includes the required characters in the result array for the password
+  for (let i=0; i < requiredChar.length; i++) {
+    result[i] = requiredChar[i];
   }
 
   return result;
